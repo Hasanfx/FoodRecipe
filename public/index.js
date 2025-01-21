82c12e878d8c6b749445998ec35a15e8eb8365a2
Hasanfx <imrshadow.fx@gmail.com>
2025-01-21 12:44:10 +0300

    first commit

---
+++ public/index.js
@@ -0,0 +1,8 @@
+// const burger = document.querySelector('#burger');
+// const menu = document.querySelector('#menu');
+
+// burger.addEventListener('click', () => {
+//     if (window.innerWidth < 768) { // Tailwind's 'md' breakpoint is 768px
+//         menu.classList.toggle('hidden');
+//     }
+// });
